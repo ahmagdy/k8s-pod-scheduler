f84af0097a92340156a28ea19597918f4f3bd18c
Ahmed Magdy <ahmedmagdi@outlook.com>
2020-06-07 20:01:54 +0200

    add job proto idl

---
+++ script/generate_grpc.sh
@@ -0,0 +1,1 @@
+protoc --go_out=plugins=grpc:. idl/job.proto
---
+++ server/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	job "github.com/Ahmad-Magdy/k8s-pod-scheduler/job"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	_port = ":8080"
+)
+
+type server struct{}
+
+func (s *server) Add(ctx context.Context, req *job.AddJobRequest) (*job.AddJobResponse, error) {
+	// TODO: Validate input fields
+	j := job.JobToSchedulerJob(req.Job)
+	fmt.Println(j.Name)
+
+	return nil, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", _port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	job.RegisterJobServiceServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
