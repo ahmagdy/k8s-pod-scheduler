451e72d42a024db35bb797af33f2ef792edd3a99
Ahmed Magdy <ahmedmagdi@outlook.com>
2020-06-07 20:03:51 +0200

    use SchedulerJob instead of the generated model

--- scheduler/add.go
+++ scheduler/add.go
@@ -8,12 +8,12 @@ import (
 	"github.com/Ahmad-Magdy/k8s-pod-scheduler/job"
 )
 
-func (c *CronScheduler) Add(job *job.Job) error {
+func (c *CronScheduler) Add(job *job.SchedulerJob) error {
 	id, err := c.cron.AddFunc(job.Cron, func() {
 		// 15:04:05
 		startTime := time.Now()
-		c.log.Info("The job started", zap.String("job_name", job.Name), zap.Time("start_time", startTime),
-			zap.String("image_to_execute", job.Specs.Image), zap.String("container_args", job.Specs.Args),
+		c.log.Info("The job started", zap.String("job_name", job.Cron), zap.Time("start_time", startTime),
+			zap.String("image_to_execute", job.Image), zap.String("container_args", job.Args),
 		)
 
 		//time.Sleep(10 * time.Second)
