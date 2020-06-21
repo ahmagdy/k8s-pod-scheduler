be7cca1d54a776d8aec46b71ce8c3345f71e9b85
Ahmed Magdy <ahmedmagdi@outlook.com>
2020-06-21 19:43:36 +0200

    Add more logging to example main

--- example/main.go
+++ example/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"log"
 	"net"
+
+	"github.com/Ahmad-Magdy/k8s-pod-scheduler/logger"
+	"go.uber.org/zap"
 
 	"github.com/Ahmad-Magdy/k8s-pod-scheduler/server"
 )
@@ -12,13 +14,18 @@ const (
 )
 
 func main() {
-	server := server.New(nil, nil)
+	log := logger.New()
+	server, err := server.InitializeServer()
+	if err != nil {
+		log.Fatal("Error from init server", zap.Error(err))
+	}
 	lis, err := net.Listen("tcp", _port)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatal("failed to listen", zap.Error(err))
 	}
 
 	if err := server.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		log.Fatal("failed to serve", zap.Error(err))
 	}
+	log.Info("Serving on port", zap.String("PORT", _port))
 }
