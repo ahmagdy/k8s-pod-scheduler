168068caca8943bf59f6e169b2299c26fe1f265d
Ahmed Magdy <ahmedmagdi@outlook.com>
2020-07-05 22:35:26 +0200

    add tests for get pod with and without namespace

--- k8s/get_pod_test.go
+++ k8s/get_pod_test.go
@@ -10,31 +10,66 @@ import (
 )
 
 func TestGetPod(t *testing.T) {
+
+	tests := []struct {
+		name      string
+		podName   string
+		namespace string
+	}{
+		{
+			name:      "returns mypod given valid pod and namespace",
+			podName:   "mypod",
+			namespace: "abcd",
+		},
+		{
+			name:    "uses default namespace given pod name only",
+			podName: "anotherpod",
+		},
+	}
 	k8sClient := getK8sClientForGetPod(t)
-	namespace := "abcd"
-	podName := "mypod"
 
-	pod, err := k8sClient.GetPod(podName, namespace)
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			pod, err := k8sClient.GetPod(tc.podName, tc.namespace)
 
-	require.NoError(t, err)
-	require.NotNil(t, pod)
-	require.Equal(t, podName, pod.GetName())
+			require.NoError(t, err)
+			require.NotNil(t, pod)
+			require.Contains(t, tc.podName, pod.GetGenerateName())
+		})
+	}
 }
 
 func getK8sClientForGetPod(t *testing.T) K8S {
 	k8sClient := newTestSimpleK8s(t)
-	k8sClient.(*k8SClient).clientset = fake.NewSimpleClientset(&v1.Pod{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Pod",
-			APIVersion: "v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "mypod",
-			Namespace: "abcd",
-			Labels: map[string]string{
-				"tag": "",
+	k8sClient.(*k8SClient).clientset = fake.NewSimpleClientset(
+		&v1.Pod{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Pod",
+				APIVersion: "v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:         "mypod",
+				GenerateName: "mypod",
+				Namespace:    "abcd",
+				Labels: map[string]string{
+					"tag": "",
+				},
 			},
 		},
-	})
+		&v1.Pod{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Pod",
+				APIVersion: "v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:         "anotherpod",
+				GenerateName: "anotherpod",
+				Namespace:    "default",
+				Labels: map[string]string{
+					"tag": "",
+				},
+			},
+		},
+	)
 	return k8sClient
 }
