e7039b1ac9ef81d4df4e33daf1a2e010fec4f079
Ahmed Magdy <ahmedmagdi@outlook.com>
2020-07-18 16:54:41 +0200

    format add logs

--- scheduler/add.go
+++ scheduler/add.go
@@ -10,27 +10,30 @@ import (
 
 // Add to register a job in the scheduler
 func (c *CronScheduler) Add(job *job.SchedulerJob) (jobID string, err error) {
-	c.log.Info("Adding job",
-		zap.String("job_name", job.Name),
+	log := c.log.With(zap.String("job_name", job.Name))
+
+	log.Info("Adding job",
 		zap.String("cron_expression", job.Cron))
 
 	id, err := c.cron.AddFunc(job.Cron, func() {
 		// 15:04:05
 		startTime := time.Now()
-		c.log.Info("The job started",
-			zap.String("job_name", job.Cron),
+
+		log.Info("The job started",
 			zap.Time("start_time", startTime),
 			zap.String("image_to_execute", job.Image),
 			zap.Strings("container_args", job.Args),
 		)
+
 		name, err := c.k8s.CreatePod(job, "")
 		if err != nil {
 			c.log.Error("cron Add", zap.Error(err))
 		}
-		c.log.Info("The job has ended",
-			zap.String("job_name", job.Name),
+
+		log.Info("The job has ended",
 			zap.Duration("execution_time", time.Since(startTime).Round(time.Millisecond)),
 		)
+
 		c.k8s.WatchPod(name, "")
 	})
 	if err != nil {
