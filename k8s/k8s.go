20bd57750de2d855d68bece074343b2fd5d24bbe
Ahmed Magdy <ahmedmagdi@outlook.com>
2020-07-21 21:23:36 +0200

    Add CreateCronJob

--- k8s/k8s.go
+++ k8s/k8s.go
@@ -18,6 +18,7 @@ import (
 type K8S interface {
 	GetPod(name string, namespace string) (*v1.Pod, error)
 	GetCurrentNamespace() string
+	CreateCronJob(job *job.SchedulerJob, namespace string) (string, error)
 	CreatePod(job *job.SchedulerJob, namespace string) (string, error)
 	CreateNamespace(name string) error
 	DeletePod(name string, namespace string) error
